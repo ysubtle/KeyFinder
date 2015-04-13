64275160d5a22889b78e4c66324e4a0292e43a26
Gus Nelson <bananagus@gmail.com>
2015-04-13 11:23:13 -0500

    clean + comment comment updater

--- scripts/update_comments.py
+++ scripts/update_comments.py
@@ -13,21 +13,27 @@ def update_comments():
 	for c in comments:
 		p_select = c.xpath("p")
 		text = ''
+		# Selects all p elements in comment-body and extracts text
 		for p in p_select:
 			if p.text_content():
 				text += p.text_content()
 				text += '\n '
 
+		# Selects parent element of comment-body
 		parent = c.getparent()
+		# Grabs author element
 		a_select = parent[0]
+		# Selects author name
 		a_p_select = a_select.xpath("cite")[0]
 		if a_p_select.text_content() == None:
 			author = a_p_select[0].text_content()
 		else:
 			author = a_p_select.text_content()
 
+		# Grabs date element
 		t_select = parent[2]
 		t_p_select = t_select[0]
+		# Select date text
 		time = t_p_select.text_content()
 
 		results.append({
@@ -36,13 +42,10 @@ def update_comments():
 			'time': time
 		})
 
-	res = comments[0].xpath("p")
-
+	# Verifies # of comments
 	print len(results)
 
 	f.write(json.dumps(results, indent=4, sort_keys=True))
 	f.close()
-
-
 
 update_comments()
